4d54dbac22359300ea824f9f75ba81b90ad9c446
SENTRYHUB <bj.coriano@gmail.com>
2025-12-16 17:28:00 -0500

    Add files via upload

---
+++ main 47.lua
@@ -0,0 +1,15 @@
+KeySystem = true, -- As Of Beta 6, Luna Has officially Implemented A Key System!
+    KeySettings = {
+        Title = "VortexHax Key",
+        Subtitle = "Key System",
+        Note = "Best Key System Ever! Also, Please Use A HWID Keysystem like Pelican, Luarmor etc. that provide key strings based on your HWID since putting a simple string is very easy to bypass, the key is vortex123!",
+        SaveInRoot = false, -- Enabling will save the key in your RootFolder (YOU MUST HAVE ONE BEFORE ENABLING THIS OPTION)
+        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+        Key = {"vortex123"},
+        SecondAction = {
+            Enabled = true, -- Set to false if you do not want a second action,
+            Type = "Discord", -- Link / Discord.
+            Parameter = "DzMhjn3JfB" -- If Type is Discord, then put your invite link (DO NOT PUT DISCORD.GG/). Else, put the full link of your key system here.
+        }
+    }
+})
